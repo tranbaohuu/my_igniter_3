584f1782a3c9d97f3306350aafd4b2476a9670f8
Tran Bao Huu <tranbaohuu@gmail.com>
2015-05-19 20:03:00 +0700

    Signed-off-by: Tran Bao Huu <tranbaohuu@gmail.com>

---
+++ application/controllers/home.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tranbaohuu
+ * Date: 5/18/2015
+ * Time: 9:43 PM
+ */
+
+ class Home extends  CI_Controller
+{
+
+
+   public function  __construct()
+    {
+        parent:: __construct();
+    }
+
+
+
+    public function  index()
+    {
+        echo("<h1>Slam</h1>");
+
+    }
+
+}
+
+
---
+++ myDemo.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tranbaohuu
+ * Date: 5/18/2015
+ * Time: 8:12 PM
+ */
+
+class Pages extends CI_Controller
+{
+
+    public function view($page = 'home')
+    {
+    }
+}
